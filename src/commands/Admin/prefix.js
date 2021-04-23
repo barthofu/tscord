fb4f0506eeca0d8f565efa18fd34842f238e6a27
Bartholomé Gili <bartholome.gili@etu.univ-lyon1.fr>
2021-04-23 16:26:22 +0200

    added prefix command

---
+++ src/commands/Admin/prefix.js
@@ -0,0 +1,46 @@
+const commandParams = {
+    
+    name: "",
+    aliases: [],
+    args: [
+        {
+            name: {
+                en: "new prefix",
+                fr: "nouveau prefix"
+            },
+            variableName: "newPrefix",
+            type: "string",
+            params: {
+                length: 7
+            }
+        }
+    ],
+    desc: {
+        en: "Change the prefix of the bot (`default` for the original prefix).",
+        fr: "Change le prefix du bot (`default` pour le prefix originel)."
+    },
+    enabled: true,
+    dm: false,
+    nsfw: false,
+    memberPermission: ["ADMINISRATOR"],
+    botPermission: [],
+    owner: false,
+    cooldown: null
+
+}
+
+module.exports = class extends CommandPattern {
+
+    constructor () {
+        super(commandParams)
+    }
+
+    async run (msg, args, rawArgs, cmd) {
+
+        client.getGuild(msg.guild.id).set("prefix", args.newPrefix === 'default' ? null : args.newPrefix).write()
+        msg.react("✅")
+
+    }
+
+
+}
