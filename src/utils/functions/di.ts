81aeedbba2dbd7529a8c0366370701d3958abdfc
Bartholomé Gili <66025667+barthofu@users.noreply.github.com>
2022-05-23 14:33:04 +0000

    delete: useless DI utils functions

--- src/utils/functions/di.ts
+++
@@ -1,7 +0,0 @@
-import { container } from 'tsyringe'
-
-export function registerInstance(...instances: any): void {
-    for (const instance of instances) {
-        container.registerInstance(instance.constructor, instance);
-    }
-}
