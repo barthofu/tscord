15a1587f930171050b67f019c4f6959647a1773d
Bartholomé Gili <66025667+barthofu@users.noreply.github.com>
2022-06-21 14:44:31 +0000

    feat(services): local store using rxeta

--- src/services/Store.ts
+++ src/services/Store.ts
@@ -1,7 +1,18 @@
 import { singleton } from 'tsyringe'
+import { Store as RxStore } from 'rxeta'
+
+interface State {
+    counter: number
+}
+
+const initialState: State = {
+    counter: 0
+}
 
 @singleton()
-export class Store {
+export class Store extends RxStore<State> {
 
-    
+    constructor() {
+        super(initialState)
+    }
 }
