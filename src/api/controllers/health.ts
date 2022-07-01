0e8660709db6a92acb2a08034921113e6f8a0ff9
Bartholomé Gili <66025667+barthofu@users.noreply.github.com>
2022-07-01 13:19:21 +0000

    feat(api): health endpoints

--- src/api/controllers/health.ts
+++ src/api/controllers/health.ts
@@ -2,14 +2,15 @@ import { Get, Router } from "@discordx/koa"
 import { Client } from "discordx"
 import { Context } from "koa"
 import { injectable } from "tsyringe"
+import pidusage from "pidusage"
 
 import { Database } from "@services"
 import { Data } from "@entities"
 import { BaseController } from "@utils/classes"
 
-@Router()
+@Router({ options: { prefix: "/health" }})
 @injectable()
-export class BotController extends BaseController {
+export class HealthController extends BaseController {
 
     constructor(
         private readonly client: Client,
@@ -18,7 +19,7 @@ export class BotController extends BaseController {
         super()
     }
 
-    @Get('/healthcheck')
+    @Get('/check')
     async healthcheck(ctx: Context) {
 
         const body = {
@@ -28,5 +29,14 @@ export class BotController extends BaseController {
         }
 
         this.ok(ctx.response, body)
+    }
+
+    @Get('/usage')
+    async usage(ctx: Context) {
+
+        const stats = await pidusage(process.pid)
+        stats.memory /= (1024 * 1024)
+
+        this.ok(ctx.response, stats)
     }
 }
