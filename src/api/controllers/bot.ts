a46995451b19f9d2a340066f3f39234f3a92a1da
Bartholomé Gili <66025667+barthofu@users.noreply.github.com>
2022-07-01 09:05:45 +0000

    feat(api): bot/info returns total stats

--- src/api/controllers/bot.ts
+++ src/api/controllers/bot.ts
@@ -5,6 +5,7 @@ import { injectable } from "tsyringe"
 
 import { BaseController } from "@utils/classes"
 import { botOnline } from "@api/middlewares"
+import { Stats } from "@services"
 
 @Router({ options: { prefix: '/bot' }})
 @Middleware(
@@ -15,15 +16,17 @@ export class BotController extends BaseController {
 
     constructor(
         private readonly client: Client,
+        private readonly stats: Stats
     ) {
         super()
     }
 
-    @Get('/user')
+    @Get('/info')
     async user(ctx: Context) {
 
         const body = {
             user: this.client.user?.toJSON(),
+            stats: await this.stats.getTotalStats()
         }
 
         this.ok(ctx.response, body)
@@ -34,6 +37,16 @@ export class BotController extends BaseController {
 
         const body = {
             guilds: this.client.guilds.cache.map(guild => guild.toJSON()),
+        }
+
+        this.ok(ctx.response, body)
+    }
+
+    @Get('/users')
+    async users(ctx: Context) {
+
+        const body = {
+            users: this.client.users.cache.map(user => user.toJSON()),
         }
 
         this.ok(ctx.response, body)
