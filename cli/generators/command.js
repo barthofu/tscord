dcb7bf54d81fade8d749905de23c6758f1a98c3a
Bartholomé Gili <66025667+barthofu@users.noreply.github.com>
2022-07-05 16:10:52 +0000

    feat(cli): commands category picker

--- cli/generators/command.js
+++ cli/generators/command.js
@@ -1,17 +1,29 @@
+const fs = require('fs')
+
 module.exports = (plop) => {
+
+    const categories = fs.readdirSync('./src/commands/').filter(category => !category.includes('.'))
 
     plop.setGenerator('command', {
     
         description: 'Create a new command file',
 
-        prompts: [{
-            type: 'input',
-            name: 'name',
-            message: 'What is the name of the command file?',
-        }],
+        prompts: [
+            {
+                type: 'input',
+                name: 'name',
+                message: 'What is the name of the command file?',
+            },
+            {
+                type: 'list',
+                name: 'category',
+                message: 'Category of your command',
+                choices: categories
+            }
+        ],
         actions: [{
             type: 'add',
-            path: '../src/commands/{{camelCase name}}.ts',
+            path: '../src/commands/{{pascalCase category}}/{{camelCase name}}.ts',
             templateFile: 'templates/command.ts.hbs',
         }]
     })
