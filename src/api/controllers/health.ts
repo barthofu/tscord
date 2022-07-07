fe33b8efcc310d23d2d347c75f8e9f7de10b952f
Bartholomé Gili <66025667+barthofu@users.noreply.github.com>
2022-07-07 17:25:32 +0000

    feat(api): `health/host` endpoint

--- src/api/controllers/health.ts
+++ src/api/controllers/health.ts
@@ -3,6 +3,7 @@ import { Client } from "discordx"
 import { Context } from "koa"
 import { injectable } from "tsyringe"
 import pidusage from "pidusage"
+import osu from 'node-os-utils'
 
 import { Database } from "@services"
 import { Data } from "@entities"
@@ -11,6 +12,8 @@ import { BaseController } from "@utils/classes"
 @Router({ options: { prefix: "/health" }})
 @injectable()
 export class HealthController extends BaseController {
+
+    private _totalHostMemoryMb: number = osu.mem.totalMem()
 
     constructor(
         private readonly client: Client,
@@ -34,9 +37,32 @@ export class HealthController extends BaseController {
     @Get('/usage')
     async usage(ctx: Context) {
 
-        const stats = await pidusage(process.pid)
-        stats.memory /= (1024 * 1024)
+        const pidUsage = await pidusage(process.pid)
 
-        this.ok(ctx.response, stats)
+        const body = {
+            ...pidUsage,
+            memory: {
+                usedInMb: pidUsage.memory / (1024 * 1024),
+                percentage: pidUsage.memory / this._totalHostMemoryMb * 100
+            },
+        }
+
+        this.ok(ctx.response, body)
+    }
+
+    @Get('/host')
+    async host(ctx: Context) {
+
+        const body = {
+            cpu: await osu.cpu.usage(),
+            memory: await osu.mem.info(),
+            os: await osu.os.oos(),
+            uptime: await osu.os.uptime(),
+            hostname: await osu.os.hostname(),
+            platform: await osu.os.platform()
+            // drive: osu.drive.info(),
+        }
+
+        this.ok(ctx.response, body)
     }
 }
