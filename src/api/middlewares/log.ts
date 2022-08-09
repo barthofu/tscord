583b6eade72972c926d2d820cca8137096f73d2c
Bartholomé <bartho.gili@gmail.com>
2022-08-09 21:50:15 +0200

    fix(api): koa not handling wait in middleware

--- src/api/middlewares/log.ts
+++ src/api/middlewares/log.ts
@@ -1,25 +1,26 @@
+import { container } from "tsyringe"
 import chalk from "chalk"
 import { Context, Next } from "koa"
 
 import { Logger } from "@services"
-import { waitForDependency } from "@utils/functions"
 
-export function globalLog(ctx: Context, next: Next) {
-    waitForDependency(Logger).then(logger => {
-        // don't log anything if the request has a `logIgnore` query params
-        if (!ctx.query.logIgnore) {
-            const { method, url } = ctx.request
+const logger = container.resolve(Logger)
 
-            const message = `(API) ${method} - ${url}`
-            const chalkedMessage = `(${chalk.bold.white('API')}) ${chalk.bold.green(method)} - ${chalk.bold.blue(url)}`
+export async function globalLog(ctx: Context, next: Next) {
 
-            logger.console('info', chalkedMessage)
-            logger.file('info', message)
-            
-        } else {
-            delete ctx.query.logIgnore
-        }
+    // don't log anything if the request has a `logIgnore` query params
+    if (!ctx.query.logIgnore) {
+        const { method, url } = ctx.request
 
-        return next()
-    });
+        const message = `(API) ${method} - ${url}`
+        const chalkedMessage = `(${chalk.bold.white('API')}) ${chalk.bold.green(method)} - ${chalk.bold.blue(url)}`
+
+        logger.console('info', chalkedMessage)
+        logger.file('info', message)
+        
+    } else {
+        delete ctx.query.logIgnore
+    }
+
+    return next()
 }
