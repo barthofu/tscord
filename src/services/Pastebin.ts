9343543b6a2323fc149b93c7b76095bf040bae94
Artemus <31190188+Mr-Artemus@users.noreply.github.com>
2022-08-25 21:29:44 +0000

    feat(#13): Add new service for usage of Pastebin

---
+++ src/services/Pastebin.ts
@@ -0,0 +1,63 @@
+import { Paste, RentryClient } from "rentry-pastebin";
+import { singleton } from "tsyringe";
+import dayjs from "dayjs";
+
+import { Pastebin as PastebinEntity } from "@entities";
+import { Database } from "@services";
+import { Schedule } from '@decorators'
+
+@singleton()
+export class Pastebin {
+    private client: RentryClient = new RentryClient();
+
+    constructor(
+        private db: Database,
+    ) {
+       this.client.createToken();
+    }
+
+    private async waitForToken(): Promise<void> {
+        while(!this.client.getToken()) {
+            await new Promise(resolve => setTimeout(resolve, 100))
+        }
+    }
+
+    async createPaste(content: string, lifetime?: number): Promise<Paste | undefined> {
+        await this.waitForToken();
+
+        const paste = await this.client.createPaste({ content });
+
+        let pasteEntity = new PastebinEntity();
+        pasteEntity.id = paste.url;
+        pasteEntity.editCode = paste.editCode;
+        if(lifetime) pasteEntity.lifetime = Math.floor(lifetime)
+    
+        await this.db.getRepo(PastebinEntity).persistAndFlush(pasteEntity);
+    
+        return paste.paste;
+    }
+
+    async deletePaste(id: string): Promise<void> {
+        await this.waitForToken();
+        
+        const paste = await this.db.getRepo(PastebinEntity).findOne({ id });
+
+        if(!paste) return;
+
+        await this.client.deletePaste(id, paste.editCode);
+        await this.db.getRepo(PastebinEntity).remove(paste);
+    }
+
+    @Schedule('*/30 * * * *')
+    private async autoDelete(): Promise<void> {
+        const pastes = await this.db.getRepo(PastebinEntity).find({ lifetime: { $gt: 0 } });
+
+        for(const paste of pastes) {
+            const diff = dayjs().diff(dayjs(paste.createdAt), 'day');
+
+            if(diff >= paste.lifetime) {
+                await this.client.deletePaste(paste.id, paste.editCode);
+            }
+        }
+    }
+}
