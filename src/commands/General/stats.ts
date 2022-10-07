4a8ba51ed124cb171351afe504829a3ba298733e
Bartholomé Gili <bartho.gili@gmail.com>
2022-10-07 09:40:05 +0200

    chore(deps): bump `discordx` to `11.4.0`

--- src/commands/General/stats.ts
+++ src/commands/General/stats.ts
@@ -1,6 +1,6 @@
 import { Client } from "discordx"
 import { Category } from "@discordx/utilities"
-import { CommandInteraction, EmbedBuilder, User } from "discord.js"
+import { ApplicationCommandOptionType, CommandInteraction, EmbedBuilder, User } from "discord.js"
 import { injectable } from "tsyringe"
 import {
 	Pagination,
@@ -55,7 +55,7 @@ export default class StatsCommand {
 		name: 'stats'
 	})
 	async statsHandler(
-		@SlashOption({ name: 'days' }) days: number,
+		@SlashOption({ name: 'days', type: ApplicationCommandOptionType.Number, required: true }) days: number,
 		interaction: CommandInteraction,
 		client: Client,
 		{ localize }: InteractionData
