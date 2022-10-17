53d2b097898f39b984388ba74bffe3267fd5325a
Bartholomé Gili <bartho.gili@gmail.com>
2022-10-17 21:31:00 +0200

    chore: remove `twilio` plugin from versioning
    oops

--- src/plugins/twilio/config/twilio.ts
+++
@@ -1,9 +0,0 @@
-export const twilioConfig: TwilioPlugin.ConfigType = {
-    enabled: false,
-    debug: false,
-    apiValidateSource: false, // Set to false if you are using a reverse-proxy
-}
-
-export const twilioPhoneNumbers = [
-    "+1XXXXXXXXXX"
-] as const
--- src/plugins/twilio/utils/types/config.d.ts
+++
@@ -1,7 +0,0 @@
-declare namespace TwilioPlugin {
-    type ConfigType = {
-        enabled: boolean,
-        debug: boolean,
-        apiValidateSource: boolean,
-    }
-}
