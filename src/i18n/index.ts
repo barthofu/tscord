6ea91957d01829b77fe7a3b25e819fb528fffcf1
Mr-Artemus <31190188+Mr-Artemus@users.noreply.github.com>
2023-04-16 18:55:33 +0200

    Fix: export `BaseTranslation` and `BaseEntity`

--- src/i18n/index.ts
+++ src/i18n/index.ts
@@ -1,4 +1,4 @@
 export { L } from "./i18n-node"
 export { getLocaleFromInteraction } from "./detectors"
-export type { Locales, Translation } from "./i18n-types"
+export type { Locales, Translation, BaseTranslation } from "./i18n-types"
 export { loadedLocales, locales } from "./i18n-util"
