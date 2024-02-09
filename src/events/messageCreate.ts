523a750bf27609f298da4c37980ea148ab1efb0b
barthofu <dev.bartho@gmail.com>
2024-02-09 15:55:28 +0000

    refacto(#149): remove all eval from codebase
    for security concerns

--- src/events/messageCreate.ts
+++ src/events/messageCreate.ts
@@ -1,9 +1,7 @@
 import { ArgsOf, Client } from 'discordx'
 
-import { generalConfig } from '@/configs'
 import { Discord, Guard, On } from '@/decorators'
 import { Maintenance } from '@/guards'
-import { executeEvalFromMessage, isDev } from '@/utils/functions'
 
 @Discord()
 export default class MessageCreateEvent {
@@ -16,16 +14,6 @@ export default class MessageCreateEvent {
 		[message]: ArgsOf<'messageCreate'>,
 		client: Client
 	) {
-		// eval command
-		if (
-			message.content.startsWith(`\`\`\`${generalConfig.eval.name}`)
-			&& (
-				(!generalConfig.eval.onlyOwner && isDev(message.author.id))
-				|| (generalConfig.eval.onlyOwner && message.author.id === generalConfig.ownerId)
-			)
-		)
-			executeEvalFromMessage(message)
-
 		await client.executeCommand(message, false)
 	}
 
