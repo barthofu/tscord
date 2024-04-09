836bf5e538b7c95017ec5430b40674ba2e3e8398
barthofu <dev.bartho@gmail.com>
2024-04-09 08:26:00 +0000

    remove(deps): better-sqlite3

--- src/utils/types/database.d.ts
+++ src/utils/types/database.d.ts
@@ -4,27 +4,23 @@ type DatabaseSize = {
 }
 
 type DatabaseConfigs = {
-	'sqlite': {
+	sqlite: {
 		driver: import('@mikro-orm/sqlite').SqliteDriver
 		entityManager: import('@mikro-orm/sqlite').SqlEntityManager
 	}
-	'better-sqlite': {
-		driver: import('@mikro-orm/better-sqlite').BetterSqliteDriver
-		entityManager: import('@mikro-orm/better-sqlite').SqlEntityManager
-	}
-	'postgresql': {
+	postgresql: {
 		driver: import('@mikro-orm/postgresql').PostgreSqlDriver
 		entityManager: import('@mikro-orm/postgresql').SqlEntityManager
 	}
-	'mysql': {
+	mysql: {
 		driver: import('@mikro-orm/mysql').MySqlDriver
 		entityManager: import('@mikro-orm/mysql').SqlEntityManager
 	}
-	'mariadb': {
+	mariadb: {
 		driver: import('@mikro-orm/mariadb').MariaDbDriver
 		entityManager: import('@mikro-orm/mariadb').SqlEntityManager
 	}
-	'mongo': {
+	mongo: {
 		driver: import('@mikro-orm/mongodb').MongoDriver
 		entityManager: import('@mikro-orm/mongodb').MongoEntityManager
 	}
