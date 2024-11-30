d6ef40dadb499fbce7523591f1be89e8905b07a9
barthofu <dev.bartho@gmail.com>
2024-11-30 11:16:28 +0000

    bump: dependencies

--- mikro-orm.config.ts
+++ mikro-orm.config.ts
@@ -1,22 +1,16 @@
 // @ts-nocheck
 import 'reflect-metadata'
+import 'dotenv/config'
 
 import process from 'node:process'
 
-import { defineConfig, Options } from '@mikro-orm/core'
-
-import * as entities from '@/entities'
-import { PluginsManager } from '@/services'
-import { resolveDependency } from '@/utils/functions'
+import { DatabaseDriver, defineConfig, Options } from '@mikro-orm/core'
 
 import { mikroORMConfig } from './src/configs/database'
 
 export default async () => {
-	const pluginsManager = await resolveDependency(PluginsManager)
-	await pluginsManager.loadPlugins()
-
 	return defineConfig({
 		...mikroORMConfig[process.env.NODE_ENV || 'development'] as Options<DatabaseDriver>,
-		entities: [...Object.values(entities), ...pluginsManager.getEntities()],
+		entities: [`src/entities/*.{ts,js}`, `src/plugins/*/entities/*.{ts,js}`],
 	})
 }
